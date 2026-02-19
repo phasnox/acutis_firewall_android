a6cab87b9e85b56590bcee7f79c7aaf4abe6e584
Guillermo Castillo <phasnox@protonmail.com>
2026-02-19 18:28:29 +0100

    Bump version to 1.0.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.acutis.firewall"
         minSdk = 29
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 10003
+        versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
